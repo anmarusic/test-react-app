841d8203a6d037ad652ed7d9278f48260ca43b16
anmarusic <toni_marusic@hotmail.com>
2018-03-14 18:41:26 +0100

    feat: get color from api

--- src/App.js
+++ src/App.js
@@ -2,13 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      color: ''
+    }
+  }
+
+  componentDidMount() {
+    this.getColor().then(result => this.setState({
+      color: result[0].hex
+    }))
+  }
+
+  getColor() {
+    return fetch('http://www.colr.org/json/color/random')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        return responseJson.colors;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{this.state.color}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
